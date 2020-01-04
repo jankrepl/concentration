265794b1b4bbec1ad3718e8153709b935855d365
Jan Krepl <jankrepl@yahoo.com>
2020-01-04 16:58:30 +0100

    adjust margin of h1

--- src/components/end.js
+++ src/components/end.js
@@ -5,7 +5,7 @@ const End = ({userName, finalTime, setTime, nCards}) => {
     setTime(0); // reset timer
     return (
         <React.Fragment>
-            <h1>Congratz {userName}! Finished in {finalTime} seconds</h1>
+            <h1>{`Congratz ${userName}! Finished in ${finalTime} seconds`}</h1>
             <Firebase nCards={nCards} userName={userName} finalTime={finalTime} nEntriesToDisplay={20}/>
         </React.Fragment>)
 };
